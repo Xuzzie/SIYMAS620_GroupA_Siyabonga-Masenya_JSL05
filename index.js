87418d4cfafe71d1a9ff50301de6dc7dcfc705f0
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-07-24 17:05:16 +0200

    5 new songs added  to song array by Lapalux, V V N, Mike , King Krule and Connan Mockasin

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
 // Array of song objects. Add at least 5 songs with title, artist, and genre properties.
 const songs = [
-    { title: "Hooked on a Feeling", artist: "Blue Swede", genre: "Pop" },
-    { title: "Moonage Daydream", artist: "David Bowie", genre: "Rock" },
-    { title: "I Want You Back", artist: "The Jackson 5", genre: "Pop" },
-    { title: "Spirit in the Sky", artist: "Norman Greenbaum", genre: "Rock" },
-    { title: "Cherry Bomb", artist: "The Runaways", genre: "Rock" },
-    { title: "Escape (The Piña Colada Song)", artist: "Rupert Holmes", genre: "Pop" },
-    { title: "O-O-H Child", artist: "The Five Stairsteps", genre: "R&B" },
-    { title: "Ain't No Mountain High Enough", artist: "Marvin Gaye & Tammi Terrell", genre: "R&B" },
-    { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
-    { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
-    { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
-    // Feel free to add even more songs
+  { title: "Hooked on a Feeling", artist: "Blue Swede", genre: "Pop" },
+  { title: "Moonage Daydream", artist: "David Bowie", genre: "Rock" },
+  { title: "I Want You Back", artist: "The Jackson 5", genre: "Pop" },
+  { title: "Spirit in the Sky", artist: "Norman Greenbaum", genre: "Rock" },
+  { title: "Cherry Bomb", artist: "The Runaways", genre: "Rock" },
+  {
+    title: "Escape (The Piña Colada Song)",
+    artist: "Rupert Holmes",
+    genre: "Pop",
+  },
+  { title: "O-O-H Child", artist: "The Five Stairsteps", genre: "R&B" },
+  {
+    title: "Ain't No Mountain High Enough",
+    artist: "Marvin Gaye & Tammi Terrell",
+    genre: "R&B",
+  },
+  { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
+  { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
+  { title: "Underclass", artist: "King Krule ", genre: "Rock" },
+  { title: "Ancient Frequencies ", artist: "Lapalux", genre: "Electronic" },
+  { title: "Zaza", artist: "Mike", genre: "Hip-Hop" },
+  { title: "Stick to myself", artist: " V V N", genre: "Electronic" },
+  { title: "Momo's", artist: "Connan Mockasin ", genre: "Rock" },
+  // Feel free to add even more songs
 ];
-
 
 // Object containing each Guardian's preferred genre
 const guardians = {
-    "Star-Lord": "Rock",
-    "Gamora": "Pop",
-    // Add preferences for Drax, Rocket, and Groot
+  "Star-Lord": "Rock",
+  Gamora: "Pop",
+  // Add preferences for Drax, Rocket, and Groot
 };
 
 // Function to generate playlist based on preferred genre
 function generatePlaylist(guardians, songs) {
-    // Use the map() function to create playlists for each Guardian
-    // Your code here
+  // Use the map() function to create playlists for each Guardian
+  // Your code here
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
 generatePlaylist(guardians, songs);
-
-
