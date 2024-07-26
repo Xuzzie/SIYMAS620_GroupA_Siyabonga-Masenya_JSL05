3f4b7cdbe73478ed6b95765cc10711ebb5b10a6a
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-07-26 14:20:58 +0200

    Added note on the quality of submission

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ const guardians = {
 
 // Function to generate playlist based on preferred genre
 
+//!!!For Full transparancy ive struggled majorly and therfore I used chatgpt and viewed how students worked through the code , I did take time to understand each line but This section is my weakest and requires me to study it further .  I dont feel acomplished on this section . I have booked a private session for next week  to better understand this
+
 //I decided to rewrite the entire code since ive been struggling
 //The two parameters for this function will be the guardians name and the songs they like
 function generatePlaylist(guardians, songs) {
@@ -50,7 +52,44 @@ function generatePlaylist(guardians, songs) {
 }
 
 // Call generatePlaylist and display the playlists for each Guardian
-generatePlaylist(guardians, songs);
+
+const playlists = generatePlaylist(guardians, songs); //This line of code was missing the entire time
+console.log(playlists);
+
+//This will connect this js playlist  to the html
+const playlistsDiv = document.getElementById("playlists");
+
+// I am a bit stuck but got help on this issue of appending  throught the use of For.each
+playlists.forEach(({ guardian, playlist }) => {
+  const guardianDiv = document.createElement("div");
+  guardianDiv.className = "playlist";
+
+  //Im essentially using append to stick the code to the document
+  const guardianName = document.createElement("h2");
+  guardianName.textContent = `${guardian}'s Playlist`;
+  guardianDiv.appendChild(guardianName);
+
+  const songList = document.createElement("ul");
+  playlist.forEach((song) => {
+    const songItem = document.createElement("li");
+    songItem.className = "song";
+
+    const songTitle = document.createElement("span");
+    songTitle.className = "song-title";
+    songTitle.textContent = song.title;
+
+    const songArtist = document.createElement("span");
+    songArtist.textContent = ` by ${song.artist}`;
+
+    songItem.appendChild(songTitle);
+    songItem.appendChild(songArtist);
+    songList.appendChild(songItem);
+  });
+  guardianDiv.appendChild(songList);
+
+  // Append the Guardian's playlist div to the #playlists div
+  playlistsDiv.appendChild(guardianDiv);
+});
 
 /*function generatePlaylist(guardians, songs) {
   const playlists = {}; //Im learning that we  first need to create an empty object in which we will push each team members favorite songs ,Still a bit confused by the logic of this
